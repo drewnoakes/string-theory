f59183d3b7e0d87a85d73b429529fd4e4abf3f68
Drew Noakes <git@drewnoakes.com>
2018-12-28 09:13:36 +0000

    Add converter default field

--- StringTheory.UI/Converters.cs
+++ StringTheory.UI/Converters.cs
@@ -5,8 +5,11 @@ using System.Windows.Data;
 
 namespace StringTheory.UI
 {
+    [ValueConversion(typeof(double), typeof(string))]
     public sealed class PercentageConverter : IValueConverter
     {
+        public static readonly PercentageConverter Default = new PercentageConverter();
+        
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (value is double d && d != 0)
