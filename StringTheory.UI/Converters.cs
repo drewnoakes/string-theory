2551a5d1f81bf776ada0de42d6e56e8203bdccde
Drew Noakes <git@drewnoakes.com>
2019-02-05 14:59:35 +0000

    Update converter

--- StringTheory.UI/Converters.cs
+++ StringTheory.UI/Converters.cs
@@ -119,7 +119,7 @@ namespace StringTheory.UI
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return Binding.DoNothing;
         }
     }
 }
