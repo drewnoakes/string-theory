388e1df60bd3ee93480d126a6619ef023f9d9eac
Ethan Byrd <etbyrd@microsoft.com>
2019-02-10 13:42:58 -0800

    Double click to open referrers

--- StringTheory/UI/StringList.xaml.cs
+++ StringTheory/UI/StringList.xaml.cs
@@ -1,4 +1,8 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Collections.Generic;
+using StringTheory.Analysis;
 
 namespace StringTheory.UI
 {
@@ -15,6 +19,12 @@ namespace StringTheory.UI
         {
             get => (StringListPage) GetValue(StringListPageProperty);
             set => SetValue(StringListPageProperty, value);
+        }
+
+        private void DataGridRow_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            DataGridRow row = sender as DataGridRow;
+            StringListPage.ShowReferrersCommand.Execute(new List<StringItem>() {row.DataContext as StringItem});
         }
     }
 }
